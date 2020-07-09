81c5d0a4a6c2e4d704e1365d126be4567d50d7ac
krishanathep <krishanathep@gmail.com>
2020-07-09 15:37:43 +0700

    Updates

---
+++ precache-manifest.7b61b522c0c31e77039f58a7e8a46d96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065bbcdd42f1e5b59ac683b2e4f17607",
+    "url": "/react-covid-app/index.html"
+  },
+  {
+    "revision": "84e5fcf163d59d5d540a",
+    "url": "/react-covid-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b7e1796ea6d8749ff7b7",
+    "url": "/react-covid-app/static/js/2.03348741.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/react-covid-app/static/js/2.03348741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e5fcf163d59d5d540a",
+    "url": "/react-covid-app/static/js/main.f26ecaec.chunk.js"
+  },
+  {
+    "revision": "2347c86b36859c418c48",
+    "url": "/react-covid-app/static/js/runtime-main.11008e87.js"
+  }
+]);
